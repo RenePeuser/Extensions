931f13616b4b5cc9b3b0bd0a6612500324fdc312
RenePeuser - Rene Peuser <rene.peuser@hotmail.de>
2016-06-08 20:54:45 +0200

    change name of methods

--- Extensions/Extensions/MathExtensions.cs
+++ Extensions/Extensions/MathExtensions.cs
@@ -395,45 +395,45 @@ namespace Extensions
 
 
 
-        public static int Sum(this int val1, int val2)
+        public static int Add(this int val1, int val2)
         {
             return val1 + val2;
         }
 
-        public static uint Sum(this uint val1, uint val2)
+        public static uint Add(this uint val1, uint val2)
         {
             return val1 + val2;
         }
 
-        public static long Sum(this long val1, long val2)
+        public static long Add(this long val1, long val2)
         {
             return val1 + val2;
         }
 
-        public static ulong Sum(this ulong val1, ulong val2)
+        public static ulong Add(this ulong val1, ulong val2)
         {
             return val1 + val2;
         }
 
-        public static float Sum(this float val1, float val2)
+        public static float Add(this float val1, float val2)
         {
             return val1 + val2;
         }
 
-        public static double Sum(this double val1, double val2)
+        public static double Add(this double val1, double val2)
         {
             return val1 + val2;
         }
 
-        public static decimal Sum(this decimal val1, decimal val2)
+        public static decimal Add(this decimal val1, decimal val2)
         {
             return val1 + val2;
         }
 
-        public static Point Sum(this Point val1, Point val2)
+        public static Point Add(this Point val1, Point val2)
         {
-            var x = val1.X.Sum(val2.X);
-            var y = val1.Y.Sum(val2.Y);
+            var x = val1.X.Add(val2.X);
+            var y = val1.Y.Add(val2.Y);
             return new Point(x, y);
         }
 
