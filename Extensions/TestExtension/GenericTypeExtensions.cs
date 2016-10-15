bfe4142f1fbeaf8e105c7bfefe94f44ccaf2a0c0
Rene Peuser <rene.peuser@hotmail.de>
2016-10-15 22:26:11 +0200

    add private object extensions

---
+++ Extensions/TestExtension/GenericTypeExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System.Diagnostics.Contracts;
+using Extensions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestExtension
+{
+    public static class GenericTypeExtensions
+    {
+        public static PrivateObject ToPrivateObject<T>(this T source)
+        {
+            Contract.Requires(source.IsNotNull());
+
+            return new PrivateObject(source);
+        }
+    }
+}
